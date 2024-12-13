d69a6dfe1719c31425cb9c4e747173a20250b6c3
training-dev <contact@training-dev.fr>
2024-12-13 09:14:36 +0100

    fix parser

--- src/Math.js
+++ src/Math.js
@@ -30,26 +30,6 @@ const divide = (dividend, divisor) => {
     }
 }
 
-const executeExpression = (expression) => {
-    let sumExpression = expression.match(/([0-9]*)\+([0-9]*)/);
-    if(sumExpression !== null){
-        return sum(parseInt(sumExpression[1]),parseInt(sumExpression[2]));
-    }
-    let subExpression = expression.match(/([0-9]*)\-([0-9]*)/);
-    if(subExpression !== null){
-        return substract(parseInt(subExpression[1]),parseInt(subExpression[2]));
-    }
-    let mulExpression = expression.match(/([0-9]*)\*([0-9]*)/);
-    if(mulExpression !== null){
-        return multiply(parseInt(mulExpression[1]),parseInt(mulExpression[2]));
-    }
-    let divExpression = expression.match(/([0-9]*)\/([0-9]*)/);
-    if(divExpression !== null){
-        return divide(parseInt(divExpression[1]),parseInt(divExpression[2]));
-    }
-    return new Error("expression parse failed")
-}
-
 function checkNumber(number) {
     if(isNaN(number)){
         throw new Error("pas un nombre");
@@ -72,4 +52,4 @@ function checkNumber(number) {
 }
 
 
-module.exports = { sum, substract,multiply, divide, checkNumber };
+module.exports = { sum, substract,multiply, divide, checkNumber,executeExpression };
---
+++ src/parser.js
@@ -0,0 +1,23 @@
+const { sum, substract, multiply, divide } = require("./Math");
+
+const executeExpression = (expression) => {
+    let sumExpression = expression.match(/([0-9]*)\+([0-9]*)/);
+    if(sumExpression !== null){
+        return sum(parseInt(sumExpression[1]),parseInt(sumExpression[2]));
+    }
+    let subExpression = expression.match(/([0-9]*)\-([0-9]*)/);
+    if(subExpression !== null){
+        return substract(parseInt(subExpression[1]),parseInt(subExpression[2]));
+    }
+    let mulExpression = expression.match(/([0-9]*)\*([0-9]*)/);
+    if(mulExpression !== null){
+        return multiply(parseInt(mulExpression[1]),parseInt(mulExpression[2]));
+    }
+    let divExpression = expression.match(/([0-9]*)\/([0-9]*)/);
+    if(divExpression !== null){
+        return divide(parseInt(divExpression[1]),parseInt(divExpression[2]));
+    }
+    return new Error("expression parse failed")
+}
+
+module.exports = { executeExpression };
