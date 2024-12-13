21ce4990fae1b234fbbc6f7e753ade209948aa37
training-dev <contact@training-dev.fr>
2024-12-13 11:41:09 +0100

    fix auth and add test description without code

--- src/user.js
+++ src/user.js
@@ -1,15 +1,22 @@
-const auth = async (login,password) => {
-    let result = await fetch('http://localhost:3000/connect',{
-        method: "POST",
-        body: JSON.stringify({
-            login: login,
-            password: password
-        })
-    });
+const auth = async (login, password) => {
+    let result = null;
+    try {
+        result = await fetch('http://localhost:3000/connect', {
+            method: "POST",
+            body: JSON.stringify({
+                login: login,
+                password: password
+            })
+        });
+    } catch (error) {
+        throw error;
+    }
     let data = await result.json();
-    if(result.ok){
-        localStorage.addItem('user',JSON.stringify(data));
-    }else{
-        console.log(data.message);
+    if (result.ok) {
+        localStorage.addItem('user', JSON.stringify(data));
+    } else {
+        throw new Error(data.message);
     }
 }
+
+module.exports = { auth };
---
+++ test/User.test.js
@@ -0,0 +1,14 @@
+const {auth} = require("./../src/user.js");
+
+describe("When i try to authentified",() => {
+    test("and the api server does not respond, then i should have an error",() => {
+
+    });
+    test("and the api return an ok response, then i should store data in localstorage",() => {
+
+    });
+    test("and the api return an ko response, then i should have an error",() => {
+
+    });
+})
+})
