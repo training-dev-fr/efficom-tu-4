469121d3db2a2e735676baeae1491628da58640a
training-dev <contact@training-dev.fr>
2024-12-13 11:59:55 +0100

    test ok for auth

--- test/User.test.js
+++ test/User.test.js
@@ -1,10 +1,16 @@
 const { auth } = require("./../src/user.js");
 
+beforEach(() => {
+    jest.resetAllMocks();
+    global.localStorage = {};
+    global.localStorage.addItem = jest.fn();
+})
+
 describe("When i try to authentified", () => {
     test("and the api server does not respond, then i should have an error", () => {
 
     });
-    test("and the api return an ok response, then i should store data in localstorage", () => {
+    test("and the api return an ok response, then i should store data in localstorage", async () => {
         global.fetch = jest.fn();
         global.fetch.mockResolvedValue({
             json: jest.fn().mockResolvedValue({
@@ -14,7 +20,10 @@ describe("When i try to authentified", () => {
             }),
             ok: true
         });
-
+        let spyStorage = jest.spyOn(localStorage,'addItem');
+        await auth("aurélien","123456"); 
+        expect(global.fetch).toHaveBeenCalled();
+        expect(spyStorage).toHaveBeenCalled();
     });
     test("and the api return an ko response, then i should have an error", () => {
 
