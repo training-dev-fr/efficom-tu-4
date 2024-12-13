9522da631d9ce97c7eef49eb915c506a6c0cf4a7
training-dev <contact@training-dev.fr>
2024-12-13 11:42:34 +0100

    mock fetch

--- test/User.test.js
+++ test/User.test.js
@@ -1,14 +1,22 @@
-const {auth} = require("./../src/user.js");
+const { auth } = require("./../src/user.js");
 
-describe("When i try to authentified",() => {
-    test("and the api server does not respond, then i should have an error",() => {
-
-    });
-    test("and the api return an ok response, then i should store data in localstorage",() => {
+describe("When i try to authentified", () => {
+    test("and the api server does not respond, then i should have an error", () => {
 
     });
-    test("and the api return an ko response, then i should have an error",() => {
+    test("and the api return an ok response, then i should store data in localstorage", () => {
+        global.fetch = jest.fn();
+        global.fetch.mockResolvedValue({
+            json: jest.fn().mockResolvedValue({
+                firstname: "Aurélien",
+                lastname: "Vaast",
+                login: "avaast"
+            }),
+            ok: true
+        });
 
     });
-})
+    test("and the api return an ko response, then i should have an error", () => {
+
+    });
 })
